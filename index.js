b56f4ba6a7521f1e2823c19865c4850915d6ab75
luismo <luismmorandit@gmail.com>
2023-06-08 22:19:35 -0300

    hola?

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import mainRouter from './src/routes/index.js';
 import { initMongoDB } from './src/repository/db/initDB.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 
 const app = express();
@@ -8,11 +11,11 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(3000, () => {
+
+// eslint-disable-next-line no-undef
+app.listen(process.env.PORT || 3000, () => {
     console.log('listening on port 3000');
 });
 await initMongoDB();
 
-app.use('/', (req,res) => {
-    res.json('hola');
-}  );
+app.use('/', mainRouter);
