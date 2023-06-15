fed926d06810b7e5489bb7737d1add0ec65d7289
luismo <luismmorandit@gmail.com>
2023-06-14 23:28:24 -0300

    corss

--- index.js
+++ index.js
@@ -11,12 +11,7 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5501');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+
 app.use(cors());
 
 
